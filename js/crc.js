6707f0e143202b4687abeedb59400cfa58346078
angeltsh <YWY0719@163.com>
2015-12-18 11:07:37 +0800

    TTT
    TTT

--- js/crc.js
+++ js/crc.js
@@ -106,7 +106,7 @@ function getUniCRC(data,carry,width){
 		
 		i+=2;
 	}
-	return result.toString(16);
+	return getFormatNum(result,2,16);
 }
 
 
@@ -209,18 +209,28 @@ function getNowFormatDate() {
     var seperator2 = ":";
     var month = date.getMonth() + 1;
     var strDate = date.getDate();
+	var hours=date.getHours();
+	var minutes=date.getMinutes();
+	var seconds=date.getSeconds();
+	
     if (month >= 1 && month <= 9) {
         month = "0" + month;
     }
     if (strDate >= 0 && strDate <= 9) {
         strDate = "0" + strDate;
     }
-   // var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
-   //        + " " + date.getHours() + seperator2 + date.getMinutes()
-   //        + seperator2 + date.getSeconds();
+	if (hours >= 0 && hours <= 9) {
+		hours = "0" + hours;
+	}
+	if (minutes >= 0 && minutes <= 9) {
+		minutes = "0" + minutes;
+	}
+	if (seconds >= 0 && seconds <= 9) {
+		seconds = "0" + seconds;
+	}
 			
-	var currentdate = date.getHours() + seperator2 + date.getMinutes()
-            + seperator2 + date.getSeconds();
+	var currentdate = hours + seperator2 + minutes
+            + seperator2 + seconds;
 			
     return currentdate;
 }
